c150d0d436d68d4dacab009adf5cf6645b960123
Gunnar Hellekson <gunnar@hellekson.com>
2014-03-08 04:22:19 +0000

    yes.sh now updates the date and time of the YES entry in the RSS feed

--- yes.sh
+++ yes.sh
@@ -1,3 +1,5 @@
+# replace first occurrance of <pubDate> with the current date, so we're accurate.
+sed -i '0,/<pubDate>/s|<pubDate>\(.*\)</pubDate>|<pubDate>'"`date -R`"'</pubDate>|' rss.yes.xml
 ln -sf index.yes.html index.html
 ln -sf rss.yes.xml rss.xml
 
